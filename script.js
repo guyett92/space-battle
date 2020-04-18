d4d760e4a46ffcba9c1c749362a432896127da78
guyett92 <aaronguyett@yahoo.com>
2020-04-18 13:12:14 -0700

    adds object and game logic

--- script.js
+++ script.js
@@ -6,40 +6,87 @@ class Spaceship {
         this.accuracy = accuracy;
         this.alive = true;
     }
-    checkShipStatus() {
-      if (this.hull <= 0) {
-        this.alive = false;
+    attackChance(accuracy) {
+      if (Math.random() < this.accuracy) {
+        return true;
       }
-    }
-    defend() {
-      let indexPos = 0;
-      if (ships[indexPos]["alive"] === true) {
-        this.hull -= ships[indexPos]["firepower"];
-      } else {
-        indexPos += 1;
-      }
-      // checkShipStatus();
-    }
-    attack() {
-      this.firepower
-    }
-}
-
-
-function randInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+    }  
   }
-
-const ussAssembly = new Spaceship("USS Assembly", 20, 5, 0.7);
-const captainAmerica = new Spaceship("USS Captain America", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10));
-const ironMan = new Spaceship("USS Ironman", randInt(3, 7), randInt(2, 5), (randInt(6, 9) / 10))
-const blackWidow = new Spaceship("USS Black Widow", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10));
-const hulk = new Spaceship("USS Hulk", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10));
-const thor = new Spaceship("USS Thor", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10));
-const hawkEye = new Spaceship("USS Hawkeye", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10));
-const loki = new Spaceship("USS Loki", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10));
-
-
-let ships = [captainAmerica, ironMan, blackWidow, hulk, thor, hawkEye, loki];
-ussAssembly.defend(ironMan);
-console.log(ussAssembly);
+  
+  
+  function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+    }
+  
+  const spaceBattle = {
+    createShips: function() {
+      this.alienShips = [
+      new Spaceship("USS Captain America", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10)),
+      new Spaceship("USS Ironman", randInt(3, 7), randInt(2, 5), (randInt(6, 9) / 10)),
+      new Spaceship("USS Black Widow", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10)),
+      new Spaceship("USS Hulk", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10)),
+      new Spaceship("USS Thor", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10)),
+      new Spaceship("USS Hawkeye", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10)),
+      new Spaceship("USS Loki", randInt(3,7), randInt(2, 5), (randInt(6, 9) / 10))
+      ];
+     this.ussAssembly = new Spaceship("USS Assembly", 20, 5, 0.7);
+    },
+    gameStart: function() {
+      this.createShips();
+      alert("Welcome to our awesome game: Space Battle!")
+      const readyToPlay = confirm(`Are you ready to embark on your journey as ${this.ussAssembly.name}?`);
+      if (readyToPlay) {
+        this.play();
+      } else {
+        alert("Maybe next time!");
+      }
+    },
+    play: function() {
+      indexVal = 0;
+      while(this.ussAssembly.hull > 0) {
+        if (amIAlive(alienShips[indexVal])){
+          this.attack(this.alienShips[indexVal]);
+          if (amIAlive(alienShips[indexVa])) {
+            this.defend(this.alienShips[indexVal]);
+          }
+        }
+        if(!amIAlive(alienShips[indexVal])) {
+          indexVal += 1;
+        }
+      }
+    },
+    attack: function(defender) {
+        console.log(`You are attacking ${defender.name}.`)
+        if (this.ussAssembly.attackChance()){
+          defender.hull -= this.ussAssembly.firepower;
+          if (defender.hull <= 0){
+            console.log(`You hit and destroyed ${defender.name}!`)
+          } else {
+              console.log(`You attacked and ${defender.name} and the hull of that ship is now ${defender.hull}.`)
+          }
+        } else {
+        console.log(`You missed hitting ${defender.name}.`)
+        }
+    },
+    defend: function(attacker) {
+      console.log(`You are being attacked by ${attacker.name}.`)
+      if (attacker.attackChance()) {
+        this.ussAssembly.hull -= attacker.firepower;
+        if (this.ussAssembly.hull > 0) { //FIXME: Make this a function
+          console.log(`They hit and your hull is now ${this.ussAssembly.hull}.`);
+        } else {
+          console.log(`You have lost.`);
+          alert("Game over.");
+        }
+      } else {
+        console.log(`They missed you.
+  Your hull value is ${this.ussAssembly.hull}.`);
+      }
+    },
+    amIAlive: function(ship) {
+      if (ship.hull > 0) {
+        return true;
+      }
+    }
+  }
+  spaceBattle.gameStart();
