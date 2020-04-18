9f031ba976f9a691a010584a1ab7efa1aaa66d67
guyett92 <aaronguyett@yahoo.com>
2020-04-18 11:42:19 -0700

    adds first update

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 class Spaceship {
-    constructor(hull, firepower, accuracy) {
+    constructor(name, hull, firepower, accuracy) {
+        this.name = name;
         this.hull = hull;
         this.firepower = firepower;
         this.accuracy = accuracy;
     }
 }
+
+
+const ussAssembly = new Spaceship("USS Assembly", 20, 5, 0.7);
+
+console.log(ussAssembly);
